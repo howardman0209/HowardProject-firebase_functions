91076bf7d588ac71526d047a4807a06cfadee3cd
Howard <howardman0209@gmail.com>
2022-12-15 11:20:06 +0800

    init test API call

--- functions/index.js
+++ functions/index.js
@@ -4,17 +4,23 @@ const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+const express = require('express');
+const cors = require('cors');
+const app = express()
+
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
 //
 
 // Take the text parameter passed to this HTTP endpoint and insert it into 
 // Firestore under the path /messages/:documentId/original
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-    // Grab the text parameter.
+
+app.get('/',async (req, res) => {
     const original = req.query.text;
     // Push the new message into Firestore using the Firebase Admin SDK.
     const writeResult = await admin.firestore().collection('messages').add({ original: original });
     // Send back a message that we've successfully written the message
-    res.json({ result: `Message with ID: ${writeResult.id} added.` });
+    res.status(200).json({ message: `Message with ID: ${writeResult.id} added.` });
 })
+
+exports.addMessage = functions.https.onRequest(app)
